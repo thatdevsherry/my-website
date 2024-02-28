895a669b11c1977099a618df65a2b6bfed9792b6
Shehriyar Qureshi <thatdevsherry@gmail.com>
2024-02-28 17:54:10 +0500

    feat: own settings

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,12 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Sherry's garden",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "thatdevsherry.pk",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
